382e5d2ec3ea33b8e5b0278cfda5d595bae3fb1a
gogohyut <cch130223@gmail.com>
2025-09-09 09:56:06 +0900

    d

---
+++ Ch02/src/sec01/exam04/ScapeTest.java
@@ -0,0 +1,17 @@
+package sec01.exam04;
+
+public class ScapeTest {
+
+	public static void main(String[] args) {
+		int a = 10;
+		//int = b = 20;
+		
+		if(a > 1);
+		  int b = a;
+	}
+    
+	for(int i = 0; i< 10; i++) {
+		int = c = a + i;
+		c = b + i;
+	}
+}
