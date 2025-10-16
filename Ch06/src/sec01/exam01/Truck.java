86faecdb7ed3b8ac2674a50ea8f629edca2fad98
gogohyut <cch130223@gmail.com>
2025-10-16 14:53:12 +0900

    1016 수업실습

---
+++ Ch06/src/sec01/exam01/Truck.java
@@ -0,0 +1,26 @@
+package sec01.exam01;
+
+public class Truck extends Car {
+	int speed;
+	int load;
+	
+	Truck(int speed, int load) {
+		super(speed);
+//		this.speed = speed;
+		this.load = load;
+		
+	}
+
+//	void ShowSpeed(int speed, int load) {
+//		System.out.println("Truck speed :" + speed);
+//	}
+	
+	void ShowSpeed() {
+		System.out.println("Truck speed :" + speed);
+	}
+//	
+//	void ShowLoad() {
+//		System.out.println("Load : " + load);
+		
+//	}
+}
