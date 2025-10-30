72e766841d192ba3caba8ea7330c0b03164bf306
gogohyut <cch130223@gmail.com>
2025-10-30 14:51:09 +0900

    1030 수업실습

---
+++ Ch06/src/game/simple/Dragon.java
@@ -0,0 +1,38 @@
+package game.simple;
+
+import java.util.Random;
+
+public class Dragon extends Enemy {
+	int attackPower;
+	int reward;
+	int winRatio;
+	
+	Dragon(int power, int reward, int winRatio) {
+		this.attackPower = power;
+		this.reward = reward;
+		this.winRatio = winRatio;
+	}
+	
+	void Attack(Player player) {
+		Random rand = new Random();
+		int val = rand.nextInt(100);
+		
+		if(val < 50) {
+			System.out.println("Dragon Win!!");
+			Damage(player);
+		}
+		else {
+			System.out.println("Player Win!!");
+			Reward(player);
+		}
+		
+	}
+	
+	void Damage(Player player) {
+		player.health -= attackPower;
+	}
+	
+	void Reward(Player player ) {
+		player.health += reward;
+	}
+}
---
+++ Ch06/src/game/simple/Mouse.java
@@ -0,0 +1,38 @@
+package game.simple;
+
+import java.util.Random;
+
+public class Mouse extends Enemy {
+	int food;
+	int reward;
+	int winRatio = 50;
+	
+	Mouse(int food, int reward, int winRatio) {
+		this.food = food;
+		this.reward = reward;
+		this.winRatio = winRatio;
+	}
+	
+	void Attack(Player player) {
+		Random rand = new Random();
+		int val = rand.nextInt(100);
+		
+		if(val < 50) {
+			System.out.println("Mouse Win!!");
+			Damage(player);
+		}
+		else {
+			System.out.println("Player Win!!");
+			Reward(player);
+		}
+		
+	}
+	
+	void Damage(Player player) {
+		player.money -= food;
+	}
+	
+	void Reward(Player player ) {
+		player.money += reward;
+	}
+}
