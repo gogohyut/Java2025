c53fea49ef484b722e82155641ae73fb88a3b32f
gogohyut <cch130223@gmail.com>
2025-12-02 10:49:37 +0900

    1202 수업실습

--- Ch11/src/exam05/Calculator.java
+++ Ch11/src/exam05/Calculator.java
@@ -7,10 +7,15 @@ public class Calculator {
 		return memory;
 	}
 	
-	public void setMemory(int memory) {
+	public synchronized void setMemory(int memory) {
 		this.memory = memory;
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {}
 	
 		System.out.println(Thread.currentThread().getName() + " : " + this.memory);
+		
 	
 	}
 
